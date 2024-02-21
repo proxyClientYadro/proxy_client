7ad1b0f7ad3fd5165aa26165943fd2bcf8fa1591
MaksymPastukh <saponexamq@gmail.com>
2024-02-21 19:52:01 +0100

    Правки клиента

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,44 +1,45 @@
-import { default as Add } from '@mui/icons-material/Add'
-import { default as Edit } from '@mui/icons-material/Edit'
+import {default as Add} from '@mui/icons-material/Add'
+import {default as DeleteIcon} from '@mui/icons-material/Delete'
 import logo from '../../assets/images/icon/BOT_X.png'
 import AppSearchPanel from '../app-search-panel/app-search-panel'
 import './app-header.css'
-import { useState } from 'react'
+import {useState} from 'react'
 
 const AppHeader = () => {
-	const [searchPanels, setSearchPanels] = useState([1])
+    const [searchPanels, setSearchPanels] = useState([1])
 
-	const addSearchPanel = () => {
-		setSearchPanels(itemSearchPanels => [
-			...itemSearchPanels,
-			itemSearchPanels.length + 1,
-		])
-	}
+    const addSearchPanel = () => {
+        setSearchPanels(itemSearchPanels => [
+            ...itemSearchPanels,
+            itemSearchPanels.length + 1,
+        ])
+    }
 
-	const closeSearchPanel = () => {
-		setSearchPanels(itemSearchPanels => {
-			if (itemSearchPanels.length > 1) {
-				return itemSearchPanels.slice(0, itemSearchPanels.length - 1)
-			}
-			return itemSearchPanels
-		})
-	}
+    const closeSearchPanel = () => {
+        setSearchPanels(itemSearchPanels => {
+            if (itemSearchPanels.length > 1) {
+                return itemSearchPanels.slice(0, itemSearchPanels.length - 1)
+            }
+            return itemSearchPanels
+        })
+    }
 
-	return (
-		<div className='header'>
-			{searchPanels.map(index => (
-				<AppSearchPanel key={index} />
-			))}
-			<button className='button' onClick={addSearchPanel}>
-				<Add size='medium' className={'addFilter'} />
-			</button>
-			{searchPanels.length > 1 && (
-				<button className='button' onClick={closeSearchPanel}>
-					<Edit size='medium' className={'closeFilter'} />
-				</button>
-			)}
-			<img src={logo} alt='logo' />
-		</div>
-	)
+    return (
+        <div className='header'>
+            {searchPanels.map(index => (
+                <AppSearchPanel key={index}/>
+            ))}
+            <button className='button' onClick={addSearchPanel}>
+                <Add size='medium' className={'addFilter'}/>
+            </button>
+            {searchPanels.length > 1 && (
+                <button className='button' onClick={closeSearchPanel}>
+                    <DeleteIcon size='medium' className={'closeFilter'}/>
+                </button>
+
+            )}
+            <img src={logo} alt='logo'/>
+        </div>
+    )
 }
 export default AppHeader
